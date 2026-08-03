bc33ce5c14bd736df6d4e41b9be1513da9322e7f
DavidFiri <firizadavid2005@gmail.com>
2026-08-03 13:44:34 +0300

    Tabele baze de date v2

--- database/001_create_iban_validations.sql
+++ database/001_create_iban_validations.sql
@@ -1,3 +1,4 @@
+-- Active: 1785308135359@@127.0.0.1@5432@IbanValidator@public
 CREATE TABLE iban_validations (
     iban                CHAR(24)        PRIMARY KEY,
     bank_code           CHAR(4)         NOT NULL REFERENCES banks(bank_code),
--- database/002_create_banks.sql
+++ database/002_create_banks.sql
@@ -1,4 +1,5 @@
+-- Active: 1785308135359@@127.0.0.1@5432@IbanValidator
 CREATE TABLE banks(
     bank_code       CHAR(4)         PRIMARY KEY,
-    bank_name       VARCHAR(255)    NOT NULL,
+    bank_name       VARCHAR(255)    NOT NULL
 );
