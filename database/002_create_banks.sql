a726a40b4df49622d24bf963cde1aaf2fd2628a3
DavidFiri <firizadavid2005@gmail.com>
2026-08-04 11:47:16 +0300

    Populare si modificare tabela banks

--- database/002_create_banks.sql
+++ database/002_create_banks.sql
@@ -1,5 +1,6 @@
 -- Active: 1785308135359@@127.0.0.1@5432@IbanValidator
 CREATE TABLE banks(
-    bank_code       CHAR(4)         PRIMARY KEY,
+    id              INTEGER         GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    bank_code       CHAR(4)         NOT NULL UNIQUE,
     bank_name       VARCHAR(255)    NOT NULL
 );
